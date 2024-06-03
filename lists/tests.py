3fcb90052e1eefc593b05664f17c4e7d5130de9a
Sana451 <sana451@mail.ru>
2024-06-03 15:13:47 +0300

    Создана модель Item, выполнены миграции. Добавлен тест модели Item (интеграционный тест)

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,11 @@
+import pytest
 from django.http import HttpRequest
 from django.urls import resolve
 from django.template.loader import render_to_string
 from django.test import Client
 from pytest_django.asserts import assertTemplateUsed
 
+from lists.models import Item
 from lists.views import home_page
 
 
@@ -18,3 +20,23 @@ def test_can_save_a_POST_request(client: Client):
     response = client.post("/", data={'item_text': 'A new list item'})
     assert 'A new list item' in response.content.decode()
     assertTemplateUsed(response, 'home.html')
+
+
+@pytest.mark.django_db
+def test_saving_and_retrieving_items():
+    """Тест сохранения и получения элементов списка"""
+    first_item = Item()
+    first_item.text = "The first (ever) list item"
+    first_item.save()
+    second_item = Item()
+    second_item.text = "Item the second"
+    second_item.save()
+
+    saved_items = Item.objects.all()
+    assert saved_items.count() == 2
+
+    first_saved_item = saved_items[0]
+    second_saved_item = saved_items[1]
+
+    assert first_saved_item.text == "The first (ever) list item"
+    assert second_saved_item.text == "Item the second"
