f78af588fbfc73f6435d861db19d35c407057210
Sana451 <sana451@mail.ru>
2024-06-03 13:07:33 +0300

    View домашней страницы теперь использует шаблон

--- lists/tests.py
+++ lists/tests.py
@@ -1,21 +1,13 @@
-from unittest import TestCase
-
 from django.http import HttpRequest
 from django.urls import resolve
+from django.template.loader import render_to_string
+
 from lists.views import home_page
 
 
-def test_root_url_resolves_to_home_page_view():
-    """Тест: корневой url преобразуется в представление домашней страницы"""
-    found = resolve("/")
-    assert found.func == home_page
-
-
-def test_home_page_returns_correct_html():
+def test_home_page_returns_correct_html(client):
     """Тест: домашняя страница возвращает правильный html"""
-    request = HttpRequest()
-    response = home_page(request)
+    response = client.get("/")
     html = response.content.decode('utf8')
-    assert html.startswith('<html>') is True
-    assert '<title>To-Do lists</title>' in html
-    assert html.endswith('</html>') is True
+    expected_html = render_to_string("home.html")
+    assert html == expected_html
--- lists/views.py
+++ lists/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 def home_page(requets):
     """Домашняя страница"""
-    return HttpResponse("<html><title>To-Do lists</title><p>To do</p></html>")
+    return render(requets, "home.html")
