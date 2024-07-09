57b711d573ebca1f59574b1105aa707595b0e4cb
Sana451 <sana451@mail.ru>
2024-07-09 11:35:02 +0300

    Ссылка выхода из системы заменена на кнопку (POST), функциональный тест login, send_email and logout.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import re_path
+from django.contrib.auth import views as auth_views
+
 from accounts import views
 
 urlpatterns = [
     re_path(r"^send_login_email$", views.send_login_email, name="send_login_email"),
     re_path(r"^login$", views.login, name="login"),
-    re_path(r"^logout$", views.logout, name="logout"),
+    re_path(r"^logout$", auth_views.LogoutView.as_view(next_page="/"), name="logout"),
 ]
--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -1,26 +1,20 @@
-import sys
-import time
 import re
-from unittest.mock import patch
 
 import pytest
-from django.test import override_settings
-from django.core import mail
+from unittest.mock import patch
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 
-from functional_tests.base import (wait_for_row_in_list_table,
-                                   browser,
-                                   get_item_input_box,
+from functional_tests.base import (browser,
                                    wait_until_presence_of_element)
 
 TEST_EMAIL = "edith@example.com"
 SUBJECT = "Superlists login link"
 
 
-# @pytest.mark.skip
 class TestLogin:
     """Тест регистрации в системе."""
+
     @patch("accounts.views.mail.send_mail")
     def test_can_get_email_link_to_log_in(self, mock_send_mail, browser, live_server):
         """Тест: можно получить ссылку по почте для регистрации."""
@@ -31,37 +25,27 @@ class TestLogin:
         browser.find_element(By.NAME, "email").send_keys(TEST_EMAIL)
         browser.find_element(By.NAME, "email").send_keys(Keys.ENTER)
         # появляется сообщение, о том, что ей на почту выслан email
-        # wait_until_presence_of_element(browser, '//p[contains(text(), "Check your email")]', By.XPATH)
         wait_until_presence_of_element(browser, '//div[contains(text(), "Check your email")]', By.XPATH)
         # Эдит проверяет свою почту и находит сообщение
-        # email = mail.outbox[0]
         _, send_mail_kwargs = mock_send_mail.call_args
         assert TEST_EMAIL in send_mail_kwargs["recipient_list"]
-        # assert TEST_EMAIL in email.to
         assert send_mail_kwargs["subject"] == SUBJECT
-        # assert email.subject == SUBJECT
-        # # Оно содержит ссылку на url-адрес
+        # Оно содержит ссылку на url-адрес
         assert "Use this link to log in site Superlists:" in send_mail_kwargs["message"]
-        # assert "Use this link to log in site Superlists:" in email.body
         url_search = re.search(r'http://.+/.+$', send_mail_kwargs["message"])
-        # url_search = re.search(r'http://.+/.+$', email.body)
         if not url_search:
-            # pytest.fail(f"Could not find url in email body: '{email.body}'")
             pytest.fail(f"Could not find url in email body: '{send_mail_kwargs['message']}'")
         url = url_search.group(0)
         assert start_url in url
         # Эдит нажимает на ссылку
         browser.get(url)
         # # Она зарегистрирована в системе!
-        wait_until_presence_of_element(browser, "Log out", By.LINK_TEXT)
+        wait_until_presence_of_element(browser, "//button[text()='Log out']", By.XPATH)
         navbar = browser.find_element(By.CSS_SELECTOR, ".navbar")
         assert TEST_EMAIL in navbar.text
-
-        # выход и повторный вход
-        # browser.find_element(By.LINK_TEXT, "Log out").click()
-        # wait_until_presence_of_element(browser, "email", By.NAME)
-        # browser.find_element(By.NAME, "email").send_keys(TEST_EMAIL)
-        # browser.find_element(By.NAME, "email").send_keys(Keys.ENTER)
-        # wait_until_presence_of_element(browser, "a[href]", By.CSS_SELECTOR)
-        # browser.find_element(By.CSS_SELECTOR, "a[href]").click()
-        # wait_until_presence_of_element(browser, "Log out", By.LINK_TEXT)
+        # Теперь она выходит из системы
+        browser.find_element(By.XPATH, "//button[text()='Log out']").click()
+        # Она вышла из системы
+        wait_until_presence_of_element(browser, "email", By.NAME)
+        navbar = browser.find_element(By.CSS_SELECTOR, ".navbar")
+        assert TEST_EMAIL not in navbar.text
